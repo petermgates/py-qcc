27ab99a078bbca4c8d1d0f295be51ec3a2525167
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-02 23:41:40 +0200

    Add IMR charts

--- pyqcc/pyqcc.py
+++ pyqcc/pyqcc.py
@@ -3,13 +3,15 @@ import numpy as np
 
 class qcc(object):
     '''
-    Quality Control Charts
+    Quality Control Charts.
+
     Base class for qcc models.
 
     TODO: Additional docstring.
     '''
 
     def __init__(self, data=None, newdata=None) -> None:
+        self.layers = []
         self.data = data
         self.newdata = newdata
         self.stats = []
