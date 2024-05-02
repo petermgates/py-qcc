680ff2bd35936b12c790f63b44e5196c57ec3dc4
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-03 00:30:11 +0200

    Update example data.

--- pyqcc/data/__init__.py
+++ pyqcc/data/__init__.py
@@ -3,4 +3,4 @@ import pandas as pd
 import os
 
 _basepath = os.path.abspath(os.path.dirname(__file__))
-pistonrings = pd.read_table(os.path.join(_basepath, 'pistonrings.txt')).values
+pistonrings = pd.read_csv(os.path.join(_basepath, 'pistonrings.csv'))
