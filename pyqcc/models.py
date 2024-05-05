676fcaaec3d8cdcfd28676d48bb73f33c48181ff
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-05 22:59:42 +0200

    Begin the qcc class.

--- pyqcc/models.py
+++ pyqcc/models.py
@@ -1,13 +1,39 @@
 """
 pyqcc.models
 ~~~~~~~~~~~~
-
 This module contains the base model classes for the qcc package.
 """
+import pandas as pd
 
 class Qcc(object):
-    def __init__(self, data=None, newdata=None) -> None:
-        self.layers = []
+    """
+    Quality Control Charts
+    ~~~~~~~~~~~~~~~~~~~~~~
+    The main class for control chart objects.\n
+    Specify the data and chart type as a minimum
+    """
+    def __init__(self,
+                 data: pd.DataFrame,
+                 chart_type: str,
+                 newdata: pd.DataFrame=None,
+                 nsigmas: int=3,
+                 conflevel: float=0.95
+                ) -> object:
         self.data = data
+        self.type = chart_type
         self.newdata = newdata
+        self.nsigmas = nsigmas
+        self.conflevel = conflevel
         self.stats = []
+
+        match chart_type:
+            case 'xbar':
+                self.gen_xbar()
+
+    def gen_xbar(self):
+        print('Generate xbar object.')
+
+from data import pistonrings
+from utils import qcc_groups
+a = qcc_groups(pistonrings[0:30], 'diameter', 'sample')
+xbar = Qcc(data=a, chart_type='xbar')
