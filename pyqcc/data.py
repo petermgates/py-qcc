80025106c40d2774f6a75740e64efa6c71bfbbca
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-06 01:11:24 +0200

    Add hardbake example data.

--- pyqcc/data.py
+++ pyqcc/data.py
@@ -10,3 +10,5 @@ _basepath = os.path.abspath(os.path.dirname(__file__))
 _datapath = 'data/'
 
 pistonrings = pd.read_csv(os.path.join(_basepath, _datapath, 'pistonrings.csv'))
+
+hardbake = pd.read_csv(os.path.join(_basepath, _datapath, 'hardbake.csv'), header=None)
