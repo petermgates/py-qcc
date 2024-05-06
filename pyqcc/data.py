4acae8d59d8ec759a528c169546e0a822f806369
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-06 12:21:42 +0200

    Updated data and attribution.

--- pyqcc/data.py
+++ pyqcc/data.py
@@ -1,6 +1,10 @@
 '''
 Example data sets, taken from the R qcc repo.
 Returns pandas dataframes.
+
+Data sourced from the R qcc package sample data and
+Introduction to Statistical Process Control by Douglas C. Montgomery,
+7th Ed. Wiley, 2012.
 '''
 
 import pandas as pd
@@ -9,6 +13,11 @@ import os
 _basepath = os.path.abspath(os.path.dirname(__file__))
 _datapath = 'data/'
 
+# Taken from the R qcc package data 
 pistonrings = pd.read_csv(os.path.join(_basepath, _datapath, 'pistonrings.csv'))
 
-hardbake = pd.read_csv(os.path.join(_basepath, _datapath, 'hardbake.csv'), header=None)
+# Taken from Montgomery table 6.1 (pre-grouped).
+flowwidth = pd.read_csv(os.path.join(_basepath, _datapath, 'flowwidth.csv'), header=None)
+
+# Taken from Montgomery table 6.3.
+pistonrings2 = pd.read_csv(os.path.join(_basepath, _datapath, 'pistonrings2.csv'))
