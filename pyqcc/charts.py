4127efdad58d03b363375b1fd98aa5b71316ddf5
petermgates <4511591+petermgates@users.noreply.github.com>
2024-05-07 23:24:31 +0200

    refactor chart classes to separate module.

--- pyqcc/charts.py
+++ pyqcc/charts.py
@@ -1,22 +1,80 @@
-from .models import Qcc
+import matplotlib.pyplot as plt
+import pandas as pd
+from tabulate import tabulate
+
+from .constants import A2
 
 class Chart(object):
     '''
     Control chart base class. 
     '''
+    fig_figsize = (12, 7) # 2d tuple
+    fig_facecolor = '#e5e5e5' # str
+    fig_hpad = 5
+    fig_linewidth = 1
 
-    def __init__(self) -> None:
-        self.layers = [self]
 
-    def __radd__(self, model):
-        '''
-        Layer chart objects with other passed objects.
-        '''
-        if isinstance(model, Qcc): # add chart object to qcc base object.
-            model.layers += self.layers
-            return model
-        self.layers.append(model) # otherwise append this chart object to existing.
-        return self
+class Xbar(Chart):
+    
+    def __init__(self,
+        data: pd.DataFrame,
+        title: str = "",
+        ) -> object:
+        self.data = data
+        self.summary_dict = self._summary_dict(self.data)
+        self.summary = self._summary(self.summary_dict)
+        self.chart = self._chart(self.data, self.summary_dict)
+
+    def _summary_dict(self, data: pd.DataFrame) -> dict:
+         """Group statistics for the data"""
+         describe = data.mean(axis=1).describe()
+         n = data.shape[1]
+         rbar = (data.max(axis=1) - data.min(axis=1)).mean()
+         xbarbar = (data.mean(axis=1)).mean()
+         stdevbar = data.std(axis=1).mean()
+         lcl = xbarbar - (A2[n] * rbar)
+         ucl = xbarbar + (A2[n] * rbar)
+
+         summary = {
+              "stats": {
+                "Min": [describe["min"]], "1st Qu.": [describe['25%']],
+                "Median": [describe['50%']], "Mean": [describe['mean']],
+                "3rd Qu.": [describe['75%']], "Max.": [describe['max']],
+              },
+              "limits": {
+                "LCL": [lcl],
+                "UCL": [ucl],
+              },
+              "group": {
+                   "Sample size": n,
+                   "Num. groups": describe['count'],
+                   "Center": describe['mean'],
+                   "Mean std. dev.": stdevbar,
+              }
+         }
+         return summary
+
+    def _summary(self, summary: dict) -> str:
+            _stats_tbl = tabulate(summary['stats'], headers='keys',
+                                tablefmt='plain', floatfmt='.5f')
+            _limits_tbl = tabulate(summary['limits'], headers='keys',
+                                tablefmt='plain', floatfmt='.5f')
+            
+            summary_str = "\nxbar chart for data\n\n"\
+            "Summary of group statistics:\n"\
+            f"{_stats_tbl}\n\n"\
+            f"Group sample size:  {summary['group']['Sample size']:.0f}\n"\
+            f"Number of groups:  {summary['group']['Num. groups']:.0f}\n"\
+            f"Center of group statistics:  {summary['group']['Center']:.5f}\n"\
+            f"Standard deviation:  {summary['group']['Mean std. dev.']:.9f}\n\n"\
+            "Control limits:\n"\
+            f"{_limits_tbl}"
+
+            return summary_str
+
+    def _chart(self, data: pd.DataFrame, summary: dict) -> object:
+         return None
+    
 
 class Imr(Chart):
     '''
--- pyqcc/models.py
+++ pyqcc/models.py
@@ -1,8 +1,8 @@
-from dataclasses import dataclass
 import pandas as pd
-import inspect
-from constants import A2, A3, c4, D3, D4
-from tabulate import tabulate
+import matplotlib.pyplot as plt
+
+from .charts import Xbar
+
 
 class Qcc:
     """The main class for control chart objects."""
@@ -17,76 +17,15 @@ class Qcc:
             self.chart_type = chart_type
 
             self._get_chart()
-            
-            
+    
+    def __repr__(self):
+         """Return summary statistics when object is printed."""
+         return self.summary
+
     def _get_chart(self):
+         """Return chart properties depending on chart type."""
          match self.chart_type:
             case 'xbar':
-                self.chart = Xbar(self.data)
+                self.summary = Xbar(self.data).summary
+                self.chart = Xbar(self.data).chart
          
-
-class Xbar:
-    def __init__(self,
-        data: pd.DataFrame,
-        title: str = "",
-        ) -> object:
-        self.data = data
-        self.summary = self._summary(self.data)
-        self.summary_str = self._summary_str(self.summary)
-
-    def _summary(self, data) -> dict:
-         """Group statistics for the data"""
-         describe = data.mean().describe()
-         n = data.shape[1]
-         rbar = (data.max(axis=1) - data.min(axis=1)).mean()
-         xbarbar = (data.mean(axis=1)).mean()
-         stdevbar = data.std(axis=1).mean()
-         lcl = xbarbar - (A2[n] * rbar)
-         ucl = xbarbar + (A2[n] * rbar)
-
-         summary = {
-              "stats": {
-                "Min": [describe["min"]], "1st Qu.": [describe['25%']],
-                "Median": [describe['50%']], "Mean": [describe['mean']],
-                "3rd Qu.": [describe['75%']], "Max.": [describe['max']],
-              },
-              "limits": {
-                "LCL": [lcl],
-                "UCL": [ucl],
-              },
-              "group": {
-                   "Sample size": n,
-                   "Num. groups": describe['count'],
-                   "Center": describe['mean'],
-                   "Mean std. dev.": stdevbar,
-              }
-         }
-         return summary
-
-    def _summary_str(self, summary) -> str:
-            _stats_tbl = tabulate(summary['stats'], headers='keys',
-                                tablefmt='plain', floatfmt='.5f')
-            _limits_tbl = tabulate(summary['limits'], headers='keys',
-                                tablefmt='plain', floatfmt='.5f')
-            
-            summary_str = "\nxbar chart for data\n\n"\
-            "Summary of group statistics:\n"\
-            f"{_stats_tbl}\n\n"\
-            f"Group sample size:  {summary['group']['Sample size']:.0f}\n"\
-            f"Number of groups:  {summary['group']['Num. groups']:.0f}\n"\
-            f"Center of group statistics:  {summary['group']['Center']:.5f}\n"\
-            f"Standard deviation:  {summary['group']['Mean std. dev.']:.9f}\n\n"\
-            "Control limits:\n"\
-            f"{_limits_tbl}"
-
-            return summary_str
-
-
-from data import pistonrings, flowwidth
-from utils import qcc_groups
-
-# diameter = qcc_groups(pistonrings[0:25], 'diameter', 'sample')
-xbar = Qcc(data=flowwidth, chart_type='xbar')
-print(xbar.chart.summary_str)
-# range = Qcc(data=flowwidth, chart_type='r')
-# xb = Xbar()
